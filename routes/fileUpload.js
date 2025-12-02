be54fc8af9633c51649efcdf5f01a2923c003a3a
shahariar270 <mdshahariar102@gmail.com>
2025-12-02 07:11:03 +0600

    update file upload route and form to use consistent naming

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const path = require('path');
 const upload = express.Router();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/'); // Files will be saved in the 'uploads/' folder
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const uploaded = multer({ storage: storage });
+
+upload.post('/upload', uploaded.single('myImage'), (req, res) => {
+  res.send('Image uploaded successfully!');
+});
+
 
 upload.get('/upload', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'pubilc', 'index.html'));
